80e7724ca9b4af7c9ce1338f7215e039b22b2341
Your Name <88072010+Razshy@users.noreply.github.com>
2025-04-16 21:47:14 -0500

    tw

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-timport { createServerClient } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
