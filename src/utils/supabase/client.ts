028f92976518c5262335c92b41cda972eb927a68
AndrewWang <andrewwang123118@gmail.com>
2025-04-18 00:45:00 -0500

    save

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -4,10 +4,34 @@ import { supabaseConfig } from './config'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export const createClient = () => {
-  const supabase = createClientComponentClient(supabaseConfig);
-  try {
-    return supabase;
-  } catch {
-    return supabase;
-  }
-};
+  const supabase = createBrowserClient(
+    supabaseConfig.url,
+    supabaseConfig.anonKey,
+    {
+      cookies: {
+        get(name) {
+          return document.cookie
+            .split('; ')
+            .find((row) => row.startsWith(`${name}=`))
+            ?.split('=')[1]
+        },
+        set(name, value, options) {
+          let cookie = `${name}=${value}`
+          if (options?.expires) {
+            cookie += `; expires=${options.expires.toUTCString()}`
+          }
+          if (options?.path) {
+            cookie += `; path=${options.path}`
+          }
+          document.cookie = cookie
+        },
+        remove(name, options) {
+          document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; ${
+            options?.path ? `path=${options.path}` : ''
+          }`
+        },
+      },
+    }
+  )
+  return supabase
+}
